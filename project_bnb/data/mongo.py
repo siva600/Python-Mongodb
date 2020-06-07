0b28c3ebf325ab788e0b61cce1a6944b9bc71259
Siva. V <9643073+siva600@users.noreply.github.com>
2020-06-06 21:33:42 -0700

    Update mongo.py

--- project_bnb/data/mongo.py
+++ project_bnb/data/mongo.py
@@ -1,16 +1,9 @@
 import mongoengine
 from mongoengine import connect
-#
-# conn = connect(
-#         db='test',
-#         username='Siva_60',
-#         password='12345',
-#         host='mongodb://admin:qwerty@localhost/production'
-#     )
-#
 
 
-client = mongoengine.connect("mongodb+srv://Siva_60:Siva@11160@cluster0-60b2o.gcp.mongodb.net/test?retryWrites=true")
+
+client = mongoengine.connect("mongodb+srv://<user_name>:<password>@cluster0-60b2o.gcp.mongodb.net/test?retryWrites=true")
 # my_db = client["test"]
 dblist = client.list_database_names()
 for item in dblist:
